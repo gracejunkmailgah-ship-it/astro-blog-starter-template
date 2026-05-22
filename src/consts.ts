dcf37a9a0c818573eebd0a785ebd56e3f0926802
gracejunkmailgah-ship-it <gracejunkmail.gah@gmail.com>
2026-05-22 13:30:07 -0400

    Update site title and description constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "By the Books Tree Care";
+export const SITE_DESCRIPTION = "ISA Certified Arborist serving Southern Maine";
